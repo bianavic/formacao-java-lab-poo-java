5ea8d428c9386f1be79ab732e9c032be631fc93e
bianavic <bianavic@gmail.com>
2023-10-19 12:39:52 -0300

    [orientacao a objetos] projeto inicializado

---
+++ src/one/digitalinovation/laboojava/entidade/Pedido.java
@@ -0,0 +1,17 @@
+package one.digitalinovation.laboojava.entidade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Classe que representa a entidade pedido, qual é a compra dos produtos por um cliente.
+ */
+public class Pedido {
+
+    //TODO Preencher esta classe
+
+    //codigo
+    //cliente
+    //produtos
+    //total
+}
